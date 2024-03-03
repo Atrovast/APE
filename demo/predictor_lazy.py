4681b839e1c00d36e97f600cdf73dfb155287509
namee <1747040870@qq.com>
2024-03-03 22:00:36 +0800

    misc

--- demo/predictor_lazy.py
+++ demo/predictor_lazy.py
@@ -245,13 +245,16 @@ class VisualizationDemo(object):
                 labels = labels[sorted_idxs]
                 h, w = sem_seg.shape
                 q = torch.zeros((h, w, 512), device='cuda')
+                all_mask = np.zeros((h, w), dtype=np.bool)
                 for label in filter(lambda lz: lz < 1203, labels):
                     mask_color = clip_pretrained.encode_text(clip.tokenize(LVIS_CLASSES[label]).cuda()).float()
                     binary_mask = (sem_seg == label)
+                    all_mask = all_mask | binary_mask
                     q[binary_mask] = mask_color
-                q = q.permute(2, 0, 1).unsqueeze(0)
-                resized = torch.nn.functional.interpolate(q, scale_factor=0.5, mode='bilinear', align_corners=False).squeeze(0)
-                torch.save(resized.half().cpu(), f"clipf/{name}.pt")
+                q[~all_mask] = clip_pretrained.encode_text(clip.tokenize(["background"]).cuda()).float()
+                q1 = q.permute(2, 0, 1)
+                q1 = torch.nn.functional.interpolate(q1.unsqueeze(0), scale_factor=0.5, mode='bilinear', align_corners=False).squeeze(0)
+                torch.save(q1.half().cpu(), f"/ssd/dsh/clipf/{name}.pt")
                 vis_output = visualizer.draw_sem_seg(sem_seg)
             if "instances" in predictions and (with_box or with_mask):
                 instances = predictions["instances"].to(self.cpu_device)
