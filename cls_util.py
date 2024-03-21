8a56c3dd81918518f7153d3353f5b42eab14ea5d
namee <1747040870@qq.com>
2024-03-21 15:36:03 +0800

    update cls

--- cls_util.py
+++ cls_util.py
@@ -8,16 +8,16 @@ coco = COCO
 lvis = LVIS_CLASSES
 i21k = I21k_NAMES.values()
 
-cls_list = list(coco + lvis)
+cls_list = list(ade + coco + lvis)
 for i in range(len(cls_list)):
     cls_list[i] = cls_list[i].split(",")[0]
     cls_list[i] = cls_list[i].replace("_", " ")
 
 cls_list = list(set(cls_list))
 
-rm_list = ['house', 'building']
-for rm in rm_list:
-    cls_list.remove(rm)
+# rm_list = ['house', 'building']
+# for rm in rm_list:
+#     cls_list.remove(rm)
 
 CLS_LIST = cls_list
 
